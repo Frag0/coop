17e3ffc48cf881539310542aea951388f01a9791
zyxO <charlesj52130@gmail.com>
2018-01-31 11:13:40 +0100

    bootstrap crea conv

--- src/components/ConversationCreation.vue
+++ src/components/ConversationCreation.vue
@@ -1,9 +1,25 @@
 <template>
-  <form @submit="creerConversation">
-   <div>
-     <input type="text" v-model="label" placeholder="Label">
-     <input type="text" v-model="topic" placeholder="Topic">
-     <input type="submit" value="Créer une conversation">
+  <form @submit="creerConversation" class="jumbotron" >
+  <div>
+      <div class="form-group">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">&#xe142;</span>
+          </div>
+          <input type="text"  class="form-control"  v-model="label" placeholder="Label">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">&#x270f;</span>
+          </div><input type="text"  class="form-control" v-model="topic" placeholder="Topic">
+        </div>
+      </div>
+     <input class="btn btn-success" type="submit" value="Créer une conversation">
+
+
      <router-link to="/conversations">Retour</router-link>
     </div>
   </form>
@@ -31,3 +47,19 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+form{
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left:50%;  
+  transform: translate(-50%,-50%);
+  width: 85%;
+}
+
+
+
+
+</style>
